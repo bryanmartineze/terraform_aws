7c095bff81dce927b26471fae82bb3400862f9e3
bryanmartineze <bryanmartineze@gmail.com>
2023-08-01 19:57:24 +0000

    clean

--- basic-vpc/main.tf
+++ basic-vpc/main.tf
@@ -1,9 +1,11 @@
 
 #Create VPC
 resource "aws_vpc" "dev" {
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   cidr_block = "10.0.0.0/16"
     tags = {
-    Name = "dev-vpc"
+    Name = "dev-vpc-tf"
   }
 }
 
@@ -18,6 +20,7 @@ resource "aws_internet_gateway" "dev" {
 #Public subnets sections
 resource "aws_subnet" "public_subnet_a" {
   vpc_id            = aws_vpc.dev.id
+  map_public_ip_on_launch = true
   enable_resource_name_dns_a_record_on_launch = true
   cidr_block        = "10.0.48.0/20"
   availability_zone = "us-east-1a"
@@ -28,6 +31,7 @@ resource "aws_subnet" "public_subnet_a" {
 
 resource "aws_subnet" "public_subnet_b" {
   vpc_id            = aws_vpc.dev.id
+  map_public_ip_on_launch = true
   enable_resource_name_dns_a_record_on_launch = true
   cidr_block        = "10.0.64.0/20"
   availability_zone = "us-east-1b"
@@ -39,6 +43,7 @@ resource "aws_subnet" "public_subnet_b" {
 
 resource "aws_subnet" "public_subnet_c" {
   vpc_id            = aws_vpc.dev.id
+  map_public_ip_on_launch = true
   enable_resource_name_dns_a_record_on_launch = true
   cidr_block        = "10.0.80.0/20"
   availability_zone = "us-east-1c"
@@ -67,7 +72,7 @@ resource "aws_subnet" "private_subnet_b" {
   }
 }
 
-#Route table creation section
+
 resource "aws_subnet" "private_subnet_c" {
   vpc_id            = aws_vpc.dev.id
   cidr_block        = "10.0.32.0/20"
@@ -77,7 +82,7 @@ resource "aws_subnet" "private_subnet_c" {
   }
 }
 
-
+#Route table creation section
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.dev.id
 
@@ -124,11 +129,13 @@ resource "aws_route_table_association" "private_route_table_association_c" {
 }
 
 #Security Group creation section
-resource "aws_security_group" "dev" {
-  name        = "dev-security-group"
-  description = "Allow ports 80, 443, 22, and 65535"
+resource "aws_security_group" "web" {
+  name        = "web-security-group"
+  description = "Allow ports 80, 443, 22"
+  vpc_id = aws_vpc.dev.id
+  
     tags = {
-    Name = "dev-security-group"
+    Name = "web-security-group"
   }
   
   ingress {
@@ -165,4 +172,61 @@ resource "aws_security_group" "dev" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "db" {
+  name        = "db-security-group"
+  description = "Allow ports 3306, 22"
+  vpc_id = aws_vpc.dev.id
+  
+    tags = {
+    Name = "web-security-group"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "bastion" {
+    name = "bastion-security-group"
+    description = "Allow ports 22"
+    vpc_id = aws_vpc.dev.id
+    
+    tags = {
+    Name = "bastion-security-group"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust this to restrict the IP range if needed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
 }
--- basic-vpc/providers.tf
+++ basic-vpc/providers.tf
@@ -1,11 +1,23 @@
 terraform {
-  required_providers {
+  cloud {
+    organization = "bryanmartineze-devops"
+
+    workspaces {
+      name = "cicd-pipeline-example"
+    }
+  }
+  
+    required_providers {
     aws = {
-      version = "= 5.6.2"
+      version = ">= 5.6.2"
     }
   }
 }
 
+variable "aws_region" {
+    
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
